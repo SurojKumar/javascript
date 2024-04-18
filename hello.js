5ade9a38c9feb126f3b255996819c4ccb40ca9b7
SurojKumar <surajmondal86291@gmail.com>
2024-04-18 20:12:46 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,19 @@
+//console.log('hello world')
+//var a=7
+//console.log(a)
+//a='test'
+//console.log(a)
+//let a=8
+//console.log(a)
+//a='test'
+//console.log(a)
+function test()
+{
+    for(var i=0;i<10;i++)
+      {
+        console.log(i)
+      }
+}
+test()
+//let a=[1,'test',2]
+//console.log(a)
